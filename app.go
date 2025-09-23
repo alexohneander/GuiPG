85bdcb826f725206fc1f17927171db47b9c42e18
Alex Wellnitz <alex.wellnitz@materna.group>
2025-09-23 22:40:30 +0200

    feat: initial commit

---
+++ app.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ProtonMail/gopenpgp/v3/crypto"
+)
+
+// App struct
+type App struct {
+	ctx context.Context
+}
+
+// NewApp creates a new App application struct
+func NewApp() *App {
+	return &App{}
+}
+
+// startup is called when the app starts. The context is saved
+// so we can call the runtime methods
+func (a *App) startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
+// Greet returns a greeting for the given name
+func (a *App) Greet(name string) string {
+	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+// Encrypt returns a encrypted string for the given message and private key
+func (a *App) Encrypt(message string, pubkey string) string {
+	pgp := crypto.PGP()
+	publicKey, err := crypto.NewKeyFromArmored(pubkey)
+	if err != nil {
+		return ""
+	}
+
+	encHandle, err := pgp.Encryption().Recipient(publicKey).New()
+	if err != nil {
+		return ""
+	}
+
+	pgpMessage, err := encHandle.Encrypt([]byte(message))
+	if err != nil {
+		return ""
+	}
+
+	armored, err := pgpMessage.ArmorBytes()
+	if err != nil {
+		return ""
+	}
+
+	fmt.Printf("%s", armored)
+	return fmt.Sprintf("%s", armored)
+}
+
+// Sign returns a signed string for the given message, private key and passphrase
+func (a *App) Sign(message string, privkey string, password string) string {
+	pgp := crypto.PGP()
+
+	passphrase := []byte(password)
+	privateKey, err := crypto.NewPrivateKeyFromArmored(privkey, passphrase)
+	if err != nil {
+		return ""
+	}
+
+	signer, err := pgp.Sign().SigningKey(privateKey).New()
+	if err != nil {
+		return ""
+	}
+
+	cleartextArmored, err := signer.SignCleartext([]byte(message))
+	if err != nil {
+		return ""
+	}
+
+	signer.ClearPrivateParams()
+
+	fmt.Printf("%s", cleartextArmored)
+	return fmt.Sprintf("%s", cleartextArmored)
+}
+
+// Decrypt returns a encrypted string for the given message and private key
+func (a *App) Decrypt(message string, privkey string, password string) string {
+	pgp := crypto.PGP()
+
+	passphrase := []byte(password)
+	privateKey, err := crypto.NewPrivateKeyFromArmored(privkey, passphrase)
+	if err != nil {
+		return ""
+	}
+
+	decHandle, err := pgp.Decryption().DecryptionKey(privateKey).New()
+	if err != nil {
+		return ""
+	}
+
+	decrypted, err := decHandle.Decrypt([]byte(message), crypto.Armor)
+	if err != nil {
+		return ""
+	}
+
+	decHandle.ClearPrivateParams()
+
+	fmt.Printf("%s", decrypted)
+	return fmt.Sprintf("%s", decrypted)
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import logo from './assets/images/logo-gnupg.png';
+import './App.css';
+import { Greet, Encrypt, Sign, Decrypt } from "../wailsjs/go/main/App";
+
+function App() {
+  const [textContent, setTextContent] = useState("Message");
+  const updateTextContent = (e: any) => setTextContent(e.target.value);
+  const [privateKey, setPrivateKey] = useState('');
+  const updatePrivateKey = (e: any) => setPrivateKey(e.target.value);
+  const [passphrase, setPassphrase] = useState('');
+  const updatePassphrase = (e: any) => setPassphrase(e.target.value);
+
+  function encrypt() {
+    // Verwende aktuellen textContent als Input für Verschlüsselung
+    Encrypt(textContent, privateKey).then((result) => {
+      setTextContent(result); // Ergebnis ins selbe Feld
+    });
+  }
+
+  function sign() {
+    Sign(textContent, privateKey, passphrase).then((result) => {
+      setTextContent(result);
+    });
+  }
+
+  function decrypt() {
+    Decrypt(textContent, privateKey, passphrase).then((result) => {
+      setTextContent(result);
+    });
+  }
+
+  return (
+    <div id="App">
+      <img src={logo} id="logo" alt="logo" />
+      <div className="result">Please enter your Private/Public -Key below 👇</div>
+      <div className="input-box">
+        <textarea
+          id="keybox"
+          onChange={updatePrivateKey}
+          className="txtarea"
+          placeholder="Public/Private Key"
+          value={privateKey}
+        />
+      </div>
+      <div className="input-box">
+        <label>Password </label>
+        <input
+          id="passphrase"
+          onChange={updatePassphrase}
+          type="password"
+          placeholder="Password"
+          value={passphrase}
+        />
+      </div>
+      <div id="result" className="result">Message / Result 👇</div>
+      <div className="input-box">
+        <textarea
+          id="msgbox"
+          className="txtarea"
+          onChange={updateTextContent}
+          placeholder="Message"
+          value={textContent}
+        />
+      </div>
+      <div id="input" className="input-box">
+        <button className="btn" onClick={encrypt}>Encrypt</button>
+        <button className="btn" onClick={sign}>Sign</button>
+        <button className="btn" onClick={decrypt}>Decrypt</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
